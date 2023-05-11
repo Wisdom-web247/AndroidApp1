bd7527d99e78e74b1c97ead7b929f68643b986fd
Wisdom Mahlangu <wizzymahlangu@gmail.com>
2023-05-11 16:30:31 +0200

    Added Validations

--- app/src/main/java/com/example/myapplication6/register.kt
+++ app/src/main/java/com/example/myapplication6/register.kt
@@ -1,10 +1,16 @@
 package com.example.myapplication6
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
 
 
 class register : AppCompatActivity() {
@@ -14,24 +20,66 @@ class register : AppCompatActivity() {
      lateinit var etUsername:EditText
      lateinit var btnRegister:Button
 
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
-        Toast.makeText(this, " has been registered", Toast.LENGTH_SHORT).show();
 
         etUsername= findViewById(R.id.etRUserName)
         etPassword= findViewById(R.id.etRPassword)
         btnRegister=findViewById(R.id.btnRegister)
 
             btnRegister.setOnClickListener {
-           //Toast.makeText(this, etUsername + " has been registered", Toast.LENGTH_SHORT).show();
-
+               // Toast.makeText(this,"Success is working", Toast.LENGTH_LONG).show();
+                registerUser()
     }
 
     }
-}
+
+    private fun registerUser() {
+        val userName: String = etUsername.getText().toString().trim()
+        val password: String = etPassword.getText().toString().trim()
+
+        if (userName.isEmpty()) {
+            etUsername.setError("Username is required")
+            etUsername.requestFocus()
+            return
+        } else if (password.isEmpty()) {
+            etPassword.setError("Password is required")
+            etPassword.requestFocus()
+            return
+        }
+
+        val call: Call<ResponseBody> =
+            RetrofitClient.getInstance().api.createUser(User(userName, password))
+
+        call.enqueue(object : Callback<ResponseBody?> {
+            override fun onResponse(call: Call<ResponseBody?>?, response: Response<ResponseBody?>) {
+                var s = ""
+                try {
+                    s = response.body()!!.string()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+                if (s == "SUCCESS") {
+                    Toast.makeText(
+                        this@register,
+                        "Successfully registered. Please login",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    startActivity(Intent(this@register, login::class.java))
+                } else {
+                    Toast.makeText(this@register, "User already exists!", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody?>, t: Throwable) {
+                Toast.makeText(this@register, t.message, Toast.LENGTH_LONG).show()
+            }
+        })
+
+            }
+
+    }
+
+
